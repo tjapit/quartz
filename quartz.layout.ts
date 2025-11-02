d80f1f55938a7e948eaad58360f0e25f67f85fc0
Timothy Japit <timothy.japit@gmail.com>
2025-11-02 21:39:14 +0700

    chore(index): update content and structure

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    Component.DesktopOnly(Component.ContentMeta()),
     Component.TagList(),
   ],
   left: [
@@ -61,7 +61,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta()
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
